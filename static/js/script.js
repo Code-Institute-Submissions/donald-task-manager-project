bc03cf5feb9bdb71b030e6c9547e612b5f2bd030
lionkiller900 <donalddaniels20@hotmail.com>
2020-10-02 21:59:38 +0000

    added tasks expandable table

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({draggable: "left"});
+    
+
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+            
   });
